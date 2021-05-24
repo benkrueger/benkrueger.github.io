edb6c23850a7f354b3a6da154c4b456c6d9e99ca
benjaminkrueger <bokkw4@umsystem.edu>
2021-05-24 16:03:06 -0500

    adding key to deploy

--- deploysite.sh
+++ deploysite.sh
@@ -3,5 +3,6 @@ mkdir benjaminkrueger
 cp *.html benjaminkrueger
 cp *.pdf benjaminkrueger
 cp *.css benjaminkrueger
+cp *.key benjaminkrueger
 rsync -a benjaminkrueger/* root@benjaminkrueger.xyz:/var/www/benjaminkrueger
 rm -rf benjaminkrueger
